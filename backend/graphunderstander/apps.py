e9927921501ee7138cf430465314c7d6d5c1f3cd
Yash Bhatkar <bhatkary@gmail.com>
2023-11-25 17:39:17 -0500

    Add boilerplate code and create understand url

---
+++ backend/graphunderstander/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GraphunderstanderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'graphunderstander'
---
+++ backend/graphunderstander/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def understand_graph(request):
+    return HttpResponse("Default graph response")
